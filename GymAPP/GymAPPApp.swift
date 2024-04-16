06bb929e6552df0c2f445cead825bfee336879ad
Sam <samlim1104@gmail.com>
2024-04-16 14:02:02 -0700

    Initial Commit

---
+++ GymAPP/GymAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAPPApp.swift
+//  GymAPP
+//
+//  Created by Samuel Lim on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GymAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
